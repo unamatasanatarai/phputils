f824c11359e9900368666563ab406c7ea337898e
unamatasanatarai <unamatasanatarai@gmail.com>
2015-04-19 13:35:24 +0200

    automagic

---
+++ src/phputils/dd.php
@@ -0,0 +1,16 @@
+<?php
+if (!function_exists('dd')) {
+    /**
+     * Wrapper for Symfony Dumper + die
+     * @author Unamata Sanatarai <unamatasanatarai@gmail.com>
+     */
+    function dd($var)
+    {
+        $callstack = debug_backtrace();
+        VarDumper::dump('called from: ' . $callstack[0]['file'] . ':'. $callstack[0]['line']);
+        foreach (func_get_args() as $var) {
+            VarDumper::dump($var);
+        }
+        die;
+    }
+}
