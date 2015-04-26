a7db3ff4e813693d9ec70199215d1ed446891d12
unamatasanatarai <unamatasanatarai@gmail.com>
2015-04-26 14:29:38 +0200

    automagic

--- src/phputils/dd.php
+++ src/phputils/dd.php
@@ -1,15 +1,31 @@
 <?php
+/**
+ * Wrapper for Symfony Dumper + die
+ *
+ * PHP Version 5
+ *
+ * @category PHP
+ * @package  Phputils
+ * @author   Unamata Sanatarai <unamatasanatarai@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/unamatasanatarai/phputils
+ */
+
 use Symfony\Component\VarDumper\VarDumper;
 
 if (!function_exists('dd')) {
+
     /**
-     * Wrapper for Symfony Dumper + die
-     * @author Unamata Sanatarai <unamatasanatarai@gmail.com>
+     * Wrapper for Symfony Dupmer + die
+     *
+     * @return null
      */
-    function dd($var)
+    function dd()
     {
         $callstack = debug_backtrace();
-        VarDumper::dump('called from: ' . $callstack[0]['file'] . ':'. $callstack[0]['line']);
+        VarDumper::dump(
+            'called from: ' . $callstack[0]['file'] . ':'. $callstack[0]['line']
+        );
         foreach (func_get_args() as $var) {
             VarDumper::dump($var);
         }
