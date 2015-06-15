455ee4ee85d98f098ea5d7c696540e25a94c564d
unamatasanatarai <unamatasanatarai@gmail.com>
2015-06-15 18:16:46 +0200

    automagic

--- src/phputils/DotSet.php
+++ src/phputils/DotSet.php
@@ -10,98 +10,104 @@
  * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @link     https://github.com/unamatasanatarai/phputils
  */
+if (!class_exists('DotSet')) {
 
-class DotSet
-{
-	public static function insert(array $data, $path, $values = null)
-	{
-		$_list =& $data;
-		$path = explode('.', $path);
-		$count = count($path);
-		$last = $count - 1;
+    /**
+     * Nested array utils
+     */
 
-		foreach ($path as $i => $key)
-		{
-			if (is_numeric($key) && intval($key) > 0 || $key === '0')
-			{
-				$key = intval($key);
-			}
+    class DotSet
+    {
+    	public static function insert(array $data, $path, $values = null)
+    	{
+    		$_list =& $data;
+    		$path = explode('.', $path);
+    		$count = count($path);
+    		$last = $count - 1;
 
-			if ($i === $last)
-			{
-				$_list[$key] = $values;
-				return $data;
-			}
+    		foreach ($path as $i => $key)
+    		{
+    			if (is_numeric($key) && intval($key) > 0 || $key === '0')
+    			{
+    				$key = intval($key);
+    			}
 
-			if ( ! isset($_list[$key]))
-			{
-				$_list[$key] = array();
-			}
+    			if ($i === $last)
+    			{
+    				$_list[$key] = $values;
+    				return $data;
+    			}
 
-			$_list =& $_list[$key];
-			if ( ! is_array($_list))
-			{
-				$_list = array();
-			}
-		}
-	}
+    			if ( ! isset($_list[$key]))
+    			{
+    				$_list[$key] = array();
+    			}
 
-	public static function get(array $data, $path)
-	{
-		if (empty($data))
-		{
-			return null;
-		}
+    			$_list =& $_list[$key];
+    			if ( ! is_array($_list))
+    			{
+    				$_list = array();
+    			}
+    		}
+    	}
 
-		if ($path === null)
-		{
-			return $data;
-		}
+    	public static function get(array $data, $path)
+    	{
+    		if (empty($data))
+    		{
+    			return null;
+    		}
 
-		$parts = explode('.', $path);
+    		if ($path === null)
+    		{
+    			return $data;
+    		}
 
-		while (($key = array_shift($parts)) !== null)
-		{
-			if (is_array($data) && isset($data[$key]))
-			{
-				$data =& $data[$key];
-			}
-			else
-			{
-				return null;
-			}
+    		$parts = explode('.', $path);
 
-		}
+    		while (($key = array_shift($parts)) !== null)
+    		{
+    			if (is_array($data) && isset($data[$key]))
+    			{
+    				$data =& $data[$key];
+    			}
+    			else
+    			{
+    				return null;
+    			}
 
-		return $data;
-	}
+    		}
 
-	public static function remove(array $data, $path)
-	{
-		$_list =& $data;
-		$path = explode('.', $path);
-		$count = count($path);
-		$last = $count - 1;
+    		return $data;
+    	}
 
-		foreach ($path as $i => $key)
-		{
-			if (is_numeric($key) && intval($key) > 0 || $key === '0')
-			{
-				$key = intval($key);
-			}
+    	public static function remove(array $data, $path)
+    	{
+    		$_list =& $data;
+    		$path = explode('.', $path);
+    		$count = count($path);
+    		$last = $count - 1;
 
-			if ($i === $last)
-			{
-				unset($_list[$key]);
-				return $data;
-			}
+    		foreach ($path as $i => $key)
+    		{
+    			if (is_numeric($key) && intval($key) > 0 || $key === '0')
+    			{
+    				$key = intval($key);
+    			}
 
-			if (!isset($_list[$key]))
-			{
-				return $data;
-			}
+    			if ($i === $last)
+    			{
+    				unset($_list[$key]);
+    				return $data;
+    			}
 
-			$_list =& $_list[$key];
-		}
-	}
+    			if (!isset($_list[$key]))
+    			{
+    				return $data;
+    			}
+
+    			$_list =& $_list[$key];
+    		}
+    	}
+    }
 }
